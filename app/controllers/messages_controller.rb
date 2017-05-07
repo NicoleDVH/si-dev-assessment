83f630e1b4b0b60b5b7b2a7de42e5d4d7aa7abc5
Nicole Hernandez <nicole_12909@yahoo.com>
2017-05-07 16:32:01 +0000

    Push to Heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -50,8 +50,8 @@ class MessagesController < ApplicationController
     end
 
     def send_sms(message_params)
-    account_sid = ENV["TWILIO_SID"] 
-    auth_token = ENV["TWILIO_TOKEN"] 
+    account_sid = "AC07e423b83cf54ba92a9e5ed363200d93" 
+    auth_token = "5752d00f2a6b96c2f1f9d2f620efb28e" 
      
     @client = Twilio::REST::Client.new account_sid, auth_token
         
